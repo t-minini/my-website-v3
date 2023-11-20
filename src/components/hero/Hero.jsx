1f8c70563f9737581811e9a2187ee78be573e232
Tulio <tulio.mminini@gmail.com>
2023-11-20 11:55:55 +0000

    hero component started

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,21 @@
 import style from './Hero.module.css';
+import photo from './../../assets/images/photo.jpeg';
 
 export function Hero() {
   return (
-    <section classStyle={style.heroContainer}>
-      <h1>Hello, I'm Tulio</h1>
-      <p>a former designer who found hsi passion for frontend development</p>
+    <section className={style.heroContainer}>
+      <div className={style.heroContainer__text}>
+        <h1>Hello, I'm Tulio</h1>
+        <p>a former designer who found his passion for frontend development</p>
+      </div>
+      <div className={style.heroContainer__img}>
+        <img src={photo} alt="" />
+      </div>
+      <div id="hero" className={style.heroContainer__arrow}>
+        <a href="#about">
+          <span></span>
+        </a>
+      </div>
     </section>
   );
 }
