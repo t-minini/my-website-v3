b7d2cb6a70cf727d73c1bbb4275d84858dc3d251
Tulio <tulio.mminini@gmail.com>
2023-11-22 20:47:27 +0000

    cursor component created

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { About } from './components/about/About';
 import { Vertical } from './components/name-vertical/Vertical';
 import { Skills } from './components/skills/Skills';
 import { ProjectsOne } from './components/projects-one/ProjectsOne';
+import { Cursor } from './components/cursor/Cursor';
 
 function App() {
   return (
     <div className={style.app}>
+      <Cursor />
       <Navbar />
       <Vertical />
       <Hero />
---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,9 @@
+import style from './Cursor.module.css';
+
+export function Cursor() {
+  return (
+    <span id="circle" className={style.circle}>
+      CURSOR
+    </span>
+  );
+}
