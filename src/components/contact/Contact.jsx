764d1e5d2fb3e64cad69810d4a799482e17a65f4
Tulio <tulio.mminini@gmail.com>
2023-11-28 20:15:13 +0000

    Contact component finished, css for all components changed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,44 +1,40 @@
 import style from './Contact.module.css';
+// import cv from './../../assets/curriculum/tulio-minini-cv-dev.pdf';
 
 export function Contact() {
   return (
     <section id="contact" className={style.contact}>
-      <div className={style.contact__first}>
-        <a
-          href="https://www.linkedin.com/in/tulio-minini/"
-          target={'_blank'}
-          rel="noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/t-minini"
-          target={'_blank'}
-          rel="noreferrer"
-        >
-          Github
-        </a>
+      <div className={style.contact__container}>
+        <h2>Let's connect.</h2>
+        <div className={style.contact__links}>
+          <a
+            href="https://www.linkedin.com/in/tulio-minini/"
+            target={'_blank'}
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/t-minini"
+            target={'_blank'}
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="mailto:tulio.mminini@gmail.com"
+            target={'_blank'}
+            rel="noreferrer"
+            title="Send me an e-mail"
+          >
+            E-mail
+          </a>
+        </div>
       </div>
-      <h2>Let's connect.</h2>
-      <div className={style.contact__second}>
-        <a
-          href="mailto:tulio.mminini@gmail.com"
-          target={'_blank'}
-          rel="noreferrer"
-          title="Send me an e-mail"
-        >
-          E-mail
-        </a>
+      <div className={style.copyright}>
         <p>
           <span>&#169;2024 Tulio Minini</span> All Rights Reserved.
         </p>
-        <a
-          // href={cvPdf}
-          download
-          target={"_blank"}
-          rel="noreferrer"
-          title="Download CV"
-        >Curriculum</a>
       </div>
     </section>
   );
