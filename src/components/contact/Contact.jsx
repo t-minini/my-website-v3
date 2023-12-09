09e20abaf684dae7e24063e1a92c7254ae472e9c
Tulio <tulio.mminini@gmail.com>
2023-12-09 21:28:35 +0000

    contact component finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,13 +10,21 @@ export function Contact() {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className={style.contact__container}>
-        <h2>
-          Let's <br />
-          connect<span>.</span>
-          <span>.</span>
-          <span>.</span>
-        </h2>
+      <div className={style.contact__text}>
+        <p>tm</p>
+        <div className={style.text__container}>
+          <h2>
+            Let's <br />
+            connect<span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </h2>
+          <p>
+            I'm currently up for some exciting <br />
+            projects. Hit me up if you're on <br /> the lookout for a front-end
+            <br /> developer. Let's chat!
+          </p>
+        </div>
         <div className={style.contact__links}>
           <a
             href="https://www.linkedin.com/in/tulio-minini/"
@@ -27,14 +35,6 @@ export function Contact() {
             LinkedIn
           </a>
           <a
-            href="https://github.com/t-minini"
-            target={'_blank'}
-            rel="noreferrer"
-            className={`hoverable ${isHovered ? style.hovered : ''}`}
-          >
-            Github
-          </a>
-          <a
             href="mailto:tulio.mminini@gmail.com"
             target={'_blank'}
             rel="noreferrer"
@@ -42,6 +42,14 @@ export function Contact() {
             className={`hoverable ${isHovered ? style.hovered : ''}`}
           >
             E-mail
+          </a>
+          <a
+            href="https://github.com/t-minini"
+            target={'_blank'}
+            rel="noreferrer"
+            className={`hoverable ${isHovered ? style.hovered : ''}`}
+          >
+            Github
           </a>
         </div>
       </div>
