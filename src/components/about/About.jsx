b532c2b421f504630b004d6355d77d27129d0a30
Tulio <tulio.mminini@gmail.com>
2023-12-19 12:00:42 +0000

    animation complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,11 @@
-import React, { useState, useRef } from 'react';
+// import React, {  } from 'react';
 import style from './About.module.css';
-
 import { motion } from 'framer-motion';
 
 export function About() {
   const text = `With a diverse background in construction, interior design architecture, and hospitality, I offer a unique perspective to my work. Originally from Brazil, now based in Dublin, I discovered my passion for front-end development in 2022, when I completed Ironhack's full-stack web development bootcamp, which equipped me with the skills to create visually appealing and user-friendly websites. As a dedicated professional, I'm committed to continuous learning to hone my professional skills and contribute to innovative projects.`;
 
-  const wordsArray = text.split(' ');
-
-  const [hoveredWords, setHoveredWords] = useState({});
-  const timeoutRef = useRef(null);
-
-  const handleWordHover = (index) => {
-    setHoveredWords((prevHoveredWords) => ({
-      ...prevHoveredWords,
-      [index]: true,
-    }));
-  };
-
-  const handleWordLeave = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    timeoutRef.current = setTimeout(() => {
-      setHoveredWords({});
-    }, 2000);
-  };
+  const words = text.split(' ');
 
   const variants = {
     initial: {
@@ -33,33 +13,34 @@ export function About() {
       y: -50,
     },
     animate: (index) => ({
+      color: 'var(--gray-lighter)',
       opacity: 1,
       y: 0,
       transition: {
-        delay: 0.05 * index,
+        delay: 0.04 * index,
         type: 'spring',
         damping: 10,
       },
     }),
   };
 
+  const hover = {
+    scale: 1.03,
+    color: 'var(--orange)',
+    transition: { type: 'spring', delay: -500 },
+  };
+
   return (
     <section id="about" className={style.about}>
       <div>
-        {wordsArray.map((word, index) => (
+        {words.map((word, index) => (
           <motion.span
             key={index}
             className={style.word}
-            style={{
-              color: hoveredWords[index]
-                ? 'var(--orange)'
-                : 'var(--gray-lighter)',
-            }}
-            onMouseOver={() => handleWordHover(index)}
-            onMouseLeave={handleWordLeave}
             variants={variants}
             initial="initial"
             whileInView="animate"
+            whileHover={hover}
             custom={index}
           >
             {index !== 0 && ' '}
