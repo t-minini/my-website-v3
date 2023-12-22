079391e972716b0019643c333b2c9820af5921c6
Tulio <tulio.mminini@gmail.com>
2023-12-22 18:41:56 +0000

    fixing projects img sizes, adding a new project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import projectsJSON from '../../projects.json';
 import style from './Projects.module.css';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
 const ProjectCard = ({ projectsJSON }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({
@@ -12,17 +14,33 @@ const ProjectCard = ({ projectsJSON }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-400, 400]);
 
   return (
-    <section className={style.container} key={projectsJSON.id}>
+    <section
+      className={style.container}
+      key={projectsJSON.id}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className={style.imageContainer} ref={ref}>
         <img loading="lazy" src={projectsJSON.img} alt="" />
       </div>
       <motion.div className={style.textContainer} style={{ y }}>
         <div className={style.topWrapper}>
           <h2>{projectsJSON.project}</h2>
-          <a href={projectsJSON.github} target={'_blank'} rel="noreferrer">
+          <a
+            href={projectsJSON.github}
+            target={'_blank'}
+            rel="noreferrer"
+            k
+            className={`hoverable ${isHovered ? style.hovered : ''}`}
+          >
             Github
           </a>
-          <a href={projectsJSON.website} target={'_blank'} rel="noreferrer">
+          <a
+            href={projectsJSON.website}
+            target={'_blank'}
+            rel="noreferrer"
+            className={`hoverable ${isHovered ? style.hovered : ''}`}
+          >
             Website
           </a>
         </div>
