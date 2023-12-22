871d7a496029ec8ad6044ee5a49a13f362a535bc
Tulio <tulio.mminini@gmail.com>
2023-12-22 17:20:26 +0000

    revising text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import style from './About.module.css';
 import { motion } from 'framer-motion';
 
 export function About() {
-  const text = `With a diverse background in construction, interior design architecture, and hospitality, I offer a unique perspective to my work. Originally from Brazil, now based in Dublin, I discovered my passion for front-end development in 2022, when I completed Ironhack's full-stack web development bootcamp, which equipped me with the skills to create visually appealing and user-friendly websites. As a dedicated professional, I'm committed to continuous learning to hone my professional skills and contribute to innovative projects.`;
+  const text = `With a diverse background in construction, interior design, architecture, and hospitality, I offer a unique perspective to my work. Originally from Brazil, now based in Dublin, I discovered my passion for front-end development in 2022, when I completed a full-stack web development bootcamp, which equipped me with the skills to create visually appealing and user-friendly websites. As a dedicated professional, I'm committed to continuous learning to hone my professional skills and contribute to innovative projects.`;
 
   const words = text.split(' ');
 
